594c70aec1929f0b43410faa315c46574dd89aef
Aitbih Rokaya <rokayaaitbih@gmail.com>
2023-04-04 17:34:47 +0000

    first commit

---
+++ app/src/androidTest/java/com/example/testing_with_uiautomator/ExempleInstrumentedTest2.java
@@ -0,0 +1,58 @@
+package com.example.testing_with_uiautomator;
+
+import android.os.RemoteException;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+class ExampleInstrumentedTest2{
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @After
+    public void wrapUp() {
+        System.out.println("Test done");
+    }
+    @Before
+    public void setComponent() {
+        System.out.println("Components initialize");
+    }
+    @Test
+    public void testBackKeyPress() {
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).pressBack();
+    }
+    @Test
+    public void testUi() throws RemoteException {
+        UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        if (uiDevice.isScreenOn()) {
+            uiDevice.setOrientationLeft();
+            uiDevice.openNotification();
+            uiDevice.openQuickSettings();
+            uiDevice.pressHome();
+        }
+    }
+
+    @Test
+    public void testComplexUi() throws RemoteException {
+        UiDevice uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        if (uiDevice.isScreenOn()) {
+            uiDevice.openNotification();
+            uiDevice.pressBack();
+            uiDevice.openQuickSettings();
+            uiDevice.freezeRotation();
+            uiDevice.setOrientationLeft();
+            uiDevice.setOrientationRight();
+            uiDevice.pressBack();
+            uiDevice.pressHome();
+        }
+    }
+}
